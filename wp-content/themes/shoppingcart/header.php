ce073b599fd07971523d96b839d3adc86ea4a6cf
ashish140399 <ashish140399@gmail.com>
2019-10-12 09:14:21 +0530

    commit

--- wp-content/themes/shoppingcart/header.php
+++ wp-content/themes/shoppingcart/header.php
@@ -18,6 +18,10 @@ $shoppingcart_settings = shoppingcart_get_theme_options(); ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php endif;
 wp_head(); ?>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body <?php body_class(); ?>>
 	<?php 
--- wp-content/themes/shoppingcart/index.php
+++ wp-content/themes/shoppingcart/index.php
@@ -9,6 +9,10 @@
  * @since ShoppingCart 1.0
  */
 get_header(); ?>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
